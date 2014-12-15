2bfc6f2ff51bb6daa1e983a7503b231f05230e7c
U-BSS\dkazantsev <dkazantsev@nnov-kazantsev.bss.nvg.ru>
2014-12-16 00:24:21 +0500

    16.12.2014

--- bin/1.sh
+++ bin/1.sh
@@ -1,2 +1,2 @@
 . etc/urf.conf
-find $picsdir -type f | sort > var/files_old
+find $picsdir -type f | sort | sed "s@$picsdir/@@g" > var/files_old
--- bin/2.sh
+++ bin/2.sh
@@ -1,1 +1,2 @@
-cat var/files_old | while read in; do printf "$in";printf ";"; echo -n `exiv2 -qg Exif.Photo.DateTimeOriginal "$in" 2> /dev/null | awk '{print $(NF-1)" "$NF'}`; printf "\n" ; done > var/files2
+. etc/urf.conf
+cat var/files_old |  sed "s@^@$picsdir/@g" | while read in; do printf "$in";printf ";"; echo -n `opt/exiv2 -qg Exif.Photo.DateTimeOriginal "$in" 2> /dev/null | awk '{print $(NF-1)" "$NF'}`; printf "\n" ; done > var/files2
